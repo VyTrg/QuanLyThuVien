81809b17eaf4bcd8a87d0adab161b38190c05dec
VyTrg <truongvy03.it@gmail.com>
2024-03-04 23:16:10 +0700

    day len

--- cautrucdanhmucsach.h
+++ cautrucdanhmucsach.h
@@ -2,16 +2,6 @@ struct Sach{
 	std::string masach;
 	int trangthai;
 	std::string vitri;
-	
-	Sach(){
-		
-	}
-	
-	Sach(std::string ms, int tt, std::string vt){
-		masach = ms;
-		trangthai = tt;
-		vitri = vt;
-	}
 };
 
 struct NodeDMSach{
@@ -19,8 +9,55 @@ struct NodeDMSach{
 	NodeDMSach *next;
 };
 
-typedef struct NodeDMSach* dms;
+typedef struct NodeDMSach* Nodedms;
+
+//enum TrangThai{
+//	MuonDuoc;
+//	DaMuon;
+//	DaThanhLy;
+//};
+
+//prototype
+/*
+them vao dau
+them vao sau
+them vao cuoi
+*/
+void InsertFirst_NodeDMSach(Nodedms &First, Sach &sach);
+void InsertAfter_NodeDMSach(Nodedms &First, Sach &sach);
+void InsertLast_NodeDMSach(Nodedms &First, Sach &sach);
 
 
+Nodedms CreateNodeSach(Sach &sach);
 
+Nodedms CreateNodeSach(Sach &sach){
+	Nodedms First = new NodeDMSach;
+	First->sach = sach;
+	return First;
+}
 
+void InsertFirst_NodeDMSach(Nodedms &First, Sach &sach){
+	Nodedms newnode = CreateNodeSach(sach);
+	
+	newnode->next = First;
+	First = newnode;
+}
+
+void InsertAfter_NodeDMSach(Nodedms &First, Sach &sach){
+	Nodedms newnode = CreateNodeSach(sach);
+	
+	newnode->next = First->next;
+	First->next = newnode;
+}
+
+void InsertLast_NodeDMSach(Nodedms &First, Sach &sach){
+	Nodedms newnode = CreateNodeSach(sach);
+	
+	if(First == NULL) InsertFirst_NodeDMSach(First, sach);
+	else{
+		Nodedms temp = First;
+		while(temp->next != NULL)
+			temp = temp->next;
+		InsertAfter_NodeDMSach(temp, sach);
+	}	
+}
